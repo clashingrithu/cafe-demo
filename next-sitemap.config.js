3452596613e439ad299e994aacfb41fff09b5e4b
clashingrithu <rithunandh4@gmail.com>
2025-09-04 21:21:56 +0530

    Add Google Analytics, robots.txt, sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://cafe-demo-two.vercel.app/',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
-import "@/styles/globals.css";
+import '../styles/globals.css';
+import Script from 'next/script';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-RP9RRF1967`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RP9RRF1967', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
